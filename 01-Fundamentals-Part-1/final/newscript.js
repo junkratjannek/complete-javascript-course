048123fb5affe2ed9f51845340875f5b3a169b9d
junkratjannek <jannek.kloettschen@gmx.de>
2024-01-20 10:03:54 +0100

    trying out some js

---
+++ 01-Fundamentals-Part-1/final/newscript.js
@@ -0,0 +1,23 @@
+let x = Math.max(10, 20);
+console.log(x);
+
+function myFunction() {
+  alert("Hello World");
+}
+
+function toCelsius(fahrenheit) {
+  return (5 / 9) * (fahrenheit - 32);
+}
+
+let value = toCelsius(109);
+alert(value);
+
+const car = { 
+    type: "Ferrari", 
+    model: "F40", 
+    color: "red" 
+};
+
+console.log(car.type, car.model, car.color) 
+
+
